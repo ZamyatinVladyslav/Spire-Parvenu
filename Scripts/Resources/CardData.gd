3b4eb4bf3db2e8f5c59d63ee523d02b6a29ed0b8
Zamiatin Vladyslav <zamyatinvladislav200@gmail.com>
2026-05-26 10:13:59 +0300

    Cards can be selected

--- Scripts/Resources/CardData.gd
+++ Scripts/Resources/CardData.gd
@@ -4,3 +4,12 @@ class_name CardData
 @export var card_name: String = "Name"
 @export var card_description: String = "Description"
 @export var image: Texture2D
+
+func can_be_played(battle_manager) -> bool:
+	if not check_custom_conditions(battle_manager):
+		return false
+		
+	return true
+
+func check_custom_conditions(_battle_manager) -> bool:
+	return true 
